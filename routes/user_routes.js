b5fd73a45eb2067aeb05881215384e2692de6fd6
Nathan <nd14127@bristol.ac.uk>
2017-07-19 10:42:55 +0000

    working on routes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -9,4 +9,53 @@ module.exports = function (app) {
         console.log("get request to homepage");
         res.sendFile('gui.html', {root: './public'});
     });
+
+
+    app.get('/loadprogram', function(req, res) {
+    	res.sendFile('loadprogram.html', {root: './public'});
+    });
+
+app.get('/runInstruction', function(req, res) {
+    res.sendFile(path.join(__dirname, 'runInstruction.html'));
+});
+
+app.get('/loadassembly', function(req, res) {
+    res.sendFile(path.join(__dirname, 'loadassembly.html'));
+});
+
+app.get('/resources/scripts.js', function(req, res) {
+    res.sendFile(path.join(__dirname, 'resources/scripts.js'));
+});
+
+app.get('/resources/loadProgramScript.js', function(req, res) {
+    res.sendFile(path.join(__dirname, 'resources/loadProgramScript.js'));
+});
+
+app.get('/resources/loadassemblyScript.js', function(req, res) {
+    res.sendFile(path.join(__dirname, 'resources/loadassemblyScript.js'));
+});
+
+app.get('/resources/styles.css', function(req, res) {
+    res.sendFile(path.join(__dirname, 'resources/styles.css'));
+});
+
+app.get('/resources/bootstrap.min.css', function(req, res) {
+    res.sendFile(path.join(__dirname, 'resources/bootstrap.min.css'));
+});
+
+app.get('/resources/bootstrap.min.js', function(req, res) {
+    res.sendFile(path.join(__dirname, 'resources/bootstrap.min.js'));
+});
+
+app.get('/resources/jquery.js', function(req, res) {
+    res.sendFile(path.join(__dirname, 'resources/jquery.js'));
+});
+
+app.get('/assemblySpec.pdf', function(req, res) {
+    res.sendFile(path.join(__dirname, 'assemblySpec.pdf'));
+});
+
+var lastCommand = "";
+
+
 };
--- server.js
+++ server.js
@@ -4,8 +4,21 @@
 
 const configureExpress = require("./configure/express");
 
+//Import useful libraries, use npm to install express, i2c-bus etc.
+var http = require('http');
+var url = require('url');
+var express = require('express');
+var path = require('path');
+var fs = require("fs");
+
+//var compiler = require('./compiler');
+//var gpioService = require('./gpioService');
+//var ramWriter = require('./writingToRamHelper');
+//var assembler = require('./assembler');
+
+
 const app = configureExpress();
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000')
 });
