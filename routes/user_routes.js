db4212b144cf4b72b19627fdd43d293771b43573
Nathan <nd14127@bristol.ac.uk>
2017-08-01 10:37:40 +0000

    tidy up

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -81,31 +81,3 @@ module.exports = function (app) {
         res.send(rep);
     })
 };
-
-
-
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-    console.log("cookiefound " + ca);
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
-
-function eraseCookie(name) {
-	createCookie(name,"",-1);
-}
