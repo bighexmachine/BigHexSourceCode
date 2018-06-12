2f1e1c33ca284f4a96fe6817161be7ebf19672a8
Nick Pearson <np14842@my.bristol.ac.uk>
2018-06-12 14:41:06 +0000

    Force a network time sync when initialising Pi

--- server-start.sh
+++ server-start.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-# check if the port is already in use
+
+# force a network time update
+until ping -nq -c3 8.8.8.8; do
+  echo "Waiting for network..."
+done
+
+sudo service ntp stop;
+sudo ntpd -gq;
+sudo service ntp start;
 
 MAX_LOGFILES=50
 DATE=$(date -d "today" +"%d.%m.%Y_%H.%M.%S")
 
+# check if the port is already in use
 if ! nc -z 127.0.0.1 80
 then
   mkdir -p ./logs
