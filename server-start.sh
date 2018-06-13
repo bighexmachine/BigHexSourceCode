fb780f242defe6c76163ca1236ffa36d1b60608a
Nick Pearson <np14842@my.bristol.ac.uk>
2018-06-13 08:03:24 +0000

    Prevented NTP update if there is already a server running

--- server-start.sh
+++ server-start.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# force a network time update
-until ping -nq -c3 8.8.8.8; do
-  echo "Waiting for network..."
-done
-
-sudo service ntp stop;
-sudo ntpd -gq;
-sudo service ntp start;
-
-MAX_LOGFILES=50
-DATE=$(date -d "today" +"%d.%m.%Y_%H.%M.%S")
-
 # check if the port is already in use
 if ! nc -z 127.0.0.1 80
 then
+
+  # force a network time update
+  until ping -nq -c3 8.8.8.8; do
+    echo "Waiting for network..."
+  done
+
+  sudo service ntp stop;
+  sudo ntpd -gq;
+  sudo service ntp start;
+
+  MAX_LOGFILES=50
+  DATE=$(date -d "today" +"%d.%m.%Y_%H.%M.%S")
+
   mkdir -p ./logs
   sudo node ./server.js &> ./logs/$DATE.log &
 
